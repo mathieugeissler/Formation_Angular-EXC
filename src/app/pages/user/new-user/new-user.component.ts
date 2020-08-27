321704c58b1fd35c9d3ca10242dd271b5f0e4b9c
mathieu.geissler <contact@mathieugeissler.fr>
2020-08-27 14:44:38 +0000

    forms result

--- src/app/pages/user/new-user/new-user.component.ts
+++ src/app/pages/user/new-user/new-user.component.ts
@@ -1,22 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { User } from '../../../models/users/user';
+import { UserService } from '../../../services/user.service';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
   styleUrls: ['./new-user.component.scss'],
 })
-export class NewUserComponent implements OnInit {
+export class NewUserComponent implements OnInit, OnDestroy {
   public userForm: FormGroup;
+  private newUserSubscription: Subscription;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.userForm = this.formBuilder.group({
-      name: ['', Validators.required],
+      name: [null, Validators.required],
+      age: [null, [Validators.min(1), Validators.max(99)]],
+      city: [null, [Validators.required, Validators.pattern('Strasbourg')]],
     });
   }
 
-  createUser(value: User): void {}
+  createUser(): void {
+    if (this.userForm.valid) {
+      this.newUserSubscription = this.userService
+        .createUser(this.userForm.value)
+        .pipe(tap(() => this.router.navigate(['/users'])))
+        .subscribe();
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.newUserSubscription) {
+      this.newUserSubscription.unsubscribe();
+    }
+  }
 }
